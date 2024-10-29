1c1d95026775a6869c05bd0e33249b49946274d9
Gaute <mrboobai@proton.me>
2024-10-29 08:37:48 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <WiFi.h> // WiFi library for ESP32
+
+// Replace with your network credentials
+const char* ssid = "Josh";
+const char* password = "gautegaute";
+
+void setup() {
+  Serial.begin(9600);
+  
+  // Connect to Wi-Fi
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(ssid, password);
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected to Wi-Fi!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.macAddress());
+}
+
+void loop() {
+  // Your code here
+}
