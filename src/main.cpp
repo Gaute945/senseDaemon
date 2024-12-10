3413e5a84da2d9380d93387cb98578b27815b2b7
Gaute <mrboobai@proton.me>
2024-12-10 09:08:45 +0100

    platformIO for nano and rain sensor tuned for 3.3v

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 #define RAIN_SENSOR_PIN A0  // Analog pin connected to the rain sensor
-#define LED_PIN 13          // Optional: an LED for indicating rain detection
+#define LED_PIN 2          // Optional: an LED for indicating rain detection
 
 void setup() {
   Serial.begin(9600);       // Start serial communication
@@ -13,7 +13,7 @@ void loop() {
   Serial.print("Rain Sensor Value: ");
   Serial.println(sensorValue);
 
-  if (sensorValue < 500) {      // Adjust threshold based on your testing
+  if (sensorValue < 3500) {      // Adjust threshold based on your testing
     digitalWrite(LED_PIN, HIGH); // Turn on LED if rain detected
   } else {
     digitalWrite(LED_PIN, LOW);  // Turn off LED otherwise
