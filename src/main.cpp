5c09c6487151ca29f93bd383c09aa1bad31236ca
Gaute <mrboobai@proton.me>
2025-01-20 10:38:39 +0100

    humidity function

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,29 @@
 #include <Arduino.h>
 #include <Adafruit_BMP280.h>
 #include <Wire.h>
+#include "DHT.h"
 
 #define RAIN_SENSOR_PIN A0
 #define LED_PIN 2
-#define DHT11_PIN D2
 #define SCK_PIN A5 // I2C Clock
 #define SDI_PIN A4 // I2C Data
+#define DHT11_PIN 7
 
 Adafruit_BMP280 bmp;
+DHT dht11(DHT11_PIN, DHT11);
 
 void setup() {
   Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
   Wire.begin(SDI_PIN, SCK_PIN); // Set custom I2C pins
   bmp.begin(0x77);
+  dht11.begin();
 }
 
 void loop() {
   int rainSensor = analogRead(RAIN_SENSOR_PIN);
-  Serial.print("Rain Sensor Value: ");
-  Serial.println(rainSensor);
+  //Serial.print("Rain Sensor Value: ");
+  //Serial.println(rainSensor);
 
   if (rainSensor < 3500) {
     digitalWrite(LED_PIN, HIGH);
@@ -29,19 +32,24 @@ void loop() {
   }
   
   // dht11
+  float humidity = dht11.readHumidity();
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print("% \n");
+
   // BMP280
   float temperature = bmp.readTemperature();
   float pressure = bmp.readPressure() / 100;
   float altitude = bmp.readAltitude(1019.66);
 
   if (!isnan(temperature) && !isnan(pressure) && !isnan(altitude)) {
-    Serial.print(F("Temperature = "));
-    Serial.print(temperature);
-    Serial.println(F(" *C"));
+    //Serial.print(F("Temperature = "));
+    //Serial.print(temperature);
+    //Serial.println(F(" *C"));
 
-    Serial.print(F("Pressure = "));
-    Serial.print(pressure);
-    Serial.println(F(" hPa"));
+    //Serial.print(F("Pressure = "));
+    //Serial.print(pressure);
+    //Serial.println(F(" hPa"));
 
   } else {
     Serial.println(F("Error reading from BMP280 sensor!"));
