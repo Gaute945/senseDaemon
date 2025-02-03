59cf4403b4d43abc5138568d1e897a37028cc0a5
Gaute <mrboobai@proton.me>
2025-02-03 10:38:17 +0100

    ble start

--- src/main.cpp
+++ src/main.cpp
@@ -1,96 +1,121 @@
-#include <Arduino.h>
-#include <Adafruit_BMP280.h>
-#include <Wire.h>
-#include "DHT.h"
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 
-#define RAIN_PIN A0
-#define LED_PIN 2
-#define SCK_PIN A5 // I2C Clock
-#define SDI_PIN A4 // I2C Data
-#define DHT11_PIN 7
-#define UV_PIN A3
+BLEServer* pServer = NULL;
+BLECharacteristic* pSensorCharacteristic = NULL;
+BLECharacteristic* pLedCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint32_t value = 0;
 
-Adafruit_BMP280 bmp;
-DHT dht11(DHT11_PIN, DHT11);
+const int ledPin = 2; // Use the appropriate GPIO pin for your setup
 
-float temperature = 0.0;
-float pressure = 0.0;
-float humidity = 0.0;
-int rainSensor = 0;
-int uvReading = 0;
-bool rain = false;
-const float REF_VOLTAGE = 3.3; // 3.3V
-const int ADC_RESOLUTION = 4095; // 12-bit ADC (ESP32)
-const float UV_SENSITIVITY = 0.1; // 0.1 V per (mW/cm²)
-float uvIndex = 0.0;
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+#define SERVICE_UUID        "19b10000-e8f2-537e-4f6c-d104768a1214"
+#define SENSOR_CHARACTERISTIC_UUID "19b10001-e8f2-537e-4f6c-d104768a1214"
+#define LED_CHARACTERISTIC_UUID "19b10002-e8f2-537e-4f6c-d104768a1214"
+
+class MyServerCallbacks: public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    deviceConnected = true;
+  };
+
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+  }
+};
+
+class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic* pLedCharacteristic) {
+    String value = pLedCharacteristic->getValue();
+    if (value.length() > 0) {
+      Serial.print("Characteristic event, written: ");
+      Serial.println(static_cast<int>(value[0])); // Print the integer value
+
+      int receivedValue = static_cast<int>(value[0]);
+      if (receivedValue == 1) {
+        digitalWrite(ledPin, HIGH);
+      } else {
+        digitalWrite(ledPin, LOW);
+      }
+    }
+  }
+};
 
 void setup() {
-  Serial.begin(9600);
-  Wire.begin(SDI_PIN, SCK_PIN);
-  bmp.begin(0x77);
-  dht11.begin();
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
 
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(UV_PIN, INPUT);
-}
+  // Create the BLE Device
+  BLEDevice::init("ESP32");
 
-void printSensors() {
-    Serial.print(F("Temperature = "));
-    Serial.print(temperature);
-    Serial.println(F(" *C"));
-    Serial.print(F("Pressure = "));
-    Serial.print(pressure);
-    Serial.println(F(" hPa"));
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
 
-    Serial.print("Humidity: ");
-    Serial.print(humidity);
-    Serial.println(F(" %"));
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
 
-    Serial.print("Rain Sensor Value: ");
-    Serial.println(rainSensor);
-    Serial.print("Is it raining? ");
-    Serial.println(rain);
+  // Create a BLE Characteristic
+  pSensorCharacteristic = pService->createCharacteristic(
+                      SENSOR_CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
 
-    Serial.print("UV Reading: ");
-    Serial.println(uvReading);
-    Serial.print("UV Index: ");
-    Serial.println(uvIndex);
-    Serial.print("\n");
-    Serial.print("\n");
-}
+  // Create the ON button Characteristic
+  pLedCharacteristic = pService->createCharacteristic(
+                      LED_CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_WRITE
+                    );
 
-float calculateUVIndex() {
-  int rawValue = analogRead(UV_PIN); 
-  float voltage = (rawValue * REF_VOLTAGE) / ADC_RESOLUTION;
-  float uvIntensity = voltage / UV_SENSITIVITY; // Convert to mW/cm²
-  float uvIndex = uvIntensity * 0.1; // Approximate conversion to UV Index
-    
-  return uvIndex;
+  // Register the callback for the ON button characteristic
+  pLedCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pSensorCharacteristic->addDescriptor(new BLE2902());
+  pLedCharacteristic->addDescriptor(new BLE2902());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
 }
 
 void loop() {
-  // diagnose light
-  if (rainSensor < 3500) {
-    digitalWrite(LED_PIN, HIGH);
-  } else {
-    digitalWrite(LED_PIN, LOW);
+  // notify changed value
+  if (deviceConnected) {
+    pSensorCharacteristic->setValue(String(value).c_str());
+    pSensorCharacteristic->notify();
+    value++;
+    Serial.print("New value notified: ");
+    Serial.println(value);
+    delay(3000); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
   }
-  
-  // rain sensor
-  rainSensor = analogRead(RAIN_PIN);
-  rain = (rainSensor < 3500);
-  
-  // dht11
-  humidity = dht11.readHumidity();
-  
-  // BMP280
-  temperature = bmp.readTemperature();
-  pressure = bmp.readPressure() / 100;
-
-  // UV
-  uvReading = analogRead(UV_PIN);
-  uvIndex = calculateUVIndex(); // Update UV index
-
-  printSensors();  
-  delay(1000);
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected) {
+    Serial.println("Device disconnected.");
+    delay(500); // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
+    Serial.println("Start advertising");
+    oldDeviceConnected = deviceConnected;
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected) {
+    // do stuff here on connecting
+    oldDeviceConnected = deviceConnected;
+    Serial.println("Device Connected");
+  }
 }
