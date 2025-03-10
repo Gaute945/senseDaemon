b8a35c24a9ad42f14f7e687a15277bd676947381
3nterVIPER <UseRwiktorian@gmail.com>
2025-03-10 10:07:30 +0100

    MKR ENV test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,97 +1,29 @@
-#include <Arduino.h>
-#include <Adafruit_BMP280.h>
-#include <Wire.h>
-#include "DHT.h"
+#include <ArduinoModbus.h>
+#include <ArduinoRS485.h>
 
-#define RAIN_PIN A0
-#define LED_PIN D4
-#define SCK_PIN A5 // I2C Clock
-#define SDI_PIN A4 // I2C Data
-#define DHT11_PIN D7
-#define UV_PIN A3
-
-Adafruit_BMP280 bmp;
-DHT dht11(DHT11_PIN, DHT11);
-
-float temperature = 0.0;
-float pressure = 0.0;
-float humidity = 0.0;
-int rainSensor = 0;
-int uvReading = 0;
-bool rain = false;
-const float REF_VOLTAGE = 3.3; // 3.3V
-const int ADC_RESOLUTION = 4095; // 12-bit ADC (ESP32)
-const float UV_SENSITIVITY = 0.1; // 0.1 V per (mW/cm²)
-float uvIndex = 0.0;
+#define SENSOR_ADDRESS 1  // Change this if needed
 
 void setup() {
-  Serial.begin(9600);
-  Wire.begin(SDI_PIN, SCK_PIN);
-  bmp.begin(0x77);
-  dht11.begin();
+    Serial.begin(115200);
+    while (!Serial);
 
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(UV_PIN, INPUT);
-  pinMode(RAIN_PIN, INPUT);
-}
+    Serial.println("Starting RS485...");
 
-void printSensors() {
-    Serial.print(F("Temperature = "));
-    Serial.print(temperature);
-    Serial.println(F(" *C"));
-    Serial.print(F("Pressure = "));
-    Serial.print(pressure);
-    Serial.println(F(" hPa"));
-
-    Serial.print("Humidity: ");
-    Serial.print(humidity);
-    Serial.println(F(" %"));
-
-    Serial.print("Rain Sensor Value: ");
-    Serial.println(rainSensor);
-    Serial.print("Is it raining? ");
-    Serial.println(rain);
-
-    Serial.print("UV Reading: ");
-    Serial.println(uvReading);
-    Serial.print("UV Index: ");
-    Serial.println(uvIndex);
-    Serial.print("\n");
-    Serial.print("\n");
-}
-
-float calculateUVIndex() {
-  int rawValue = analogRead(UV_PIN); 
-  float voltage = (rawValue * REF_VOLTAGE) / ADC_RESOLUTION;
-  float uvIntensity = voltage / UV_SENSITIVITY; // Convert to mW/cm²
-  float uvIndex = uvIntensity * 0.1; // Approximate conversion to UV Index
-    
-  return uvIndex;
+    if (!ModbusRTUClient.begin(9600)) {
+        Serial.println("Failed to start Modbus RTU Client!");
+        while (1);
+    }
 }
 
 void loop() {
-  // diagnose light
-  if (rainSensor < 3500) {
-    digitalWrite(LED_PIN, HIGH);
-  } else {
-    digitalWrite(LED_PIN, LOW);
-  }
-  
-  // rain sensor
-  rainSensor = analogRead(RAIN_PIN);
-  rain = (rainSensor < 3500);
-  
-  // dht11
-  humidity = dht11.readHumidity();
-  
-  // BMP280
-  temperature = bmp.readTemperature();
-  pressure = bmp.readPressure() / 100;
+    Serial.println("Requesting data...");
 
-  // UV
-  uvReading = analogRead(UV_PIN);
-  uvIndex = calculateUVIndex(); // Update UV index
+    if (ModbusRTUClient.requestFrom(SENSOR_ADDRESS, INPUT_REGISTERS, 0x0000, 1)) {
+        Serial.print("Received: ");
+        Serial.println(ModbusRTUClient.read());
+    } else {
+        Serial.println("Failed to read from sensor!");
+    }
 
-  printSensors();  
-  delay(1000);
+    delay(1000);
 }
